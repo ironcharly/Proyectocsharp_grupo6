66152053ad9034237e553f148c803c026fa4aaaf
Spinetta_Carlos <spinetta.carlosa@gmail.com>
2026-05-04 19:09:00 -0300

    Creamos el proyecto

---
+++ tp1_NeuroHealth_Grupo6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace tp1_NeuroHealth_Grupo6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
